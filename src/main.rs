3536d8ad954d6b89b9af43b6b0a5f209f69b4200
Shelby Doolittle <shelby@shelbyd.com>
2019-10-04 23:11:29 -0400

    log all notify events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use notify::{RecommendedWatcher, RecursiveMode, Result, Watcher};
+use std::time::Duration;
+
+fn main() -> Result<()> {
+    let (tx, rx) = crossbeam_channel::unbounded();
+
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(50))?;
+
+    watcher.watch(".", RecursiveMode::Recursive)?;
+
+    loop {
+        let event = rx.recv()??;
+        dbg!(event);
+    }
 }
